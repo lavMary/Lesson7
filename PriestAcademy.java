c38f5c973b80e08add67e865ece8ae4b1f82c259
Mariia <49761493+lavMary@users.noreply.github.com>
2021-09-12 14:53:14 +0300

    Update PriestAcademy.java

--- PriestAcademy.java
+++ PriestAcademy.java
@@ -1,11 +1,12 @@
 public class PriestAcademy extends Academy{
-    public void teach(Trainee trainee) {
-        if (trainee instanceof Healer) {
-            super.teach(trainee);
-            //return;
+    @Override
+    public void teach(Trainee druid) {
+        if (druid instanceof Healer) {
+            super.teach(druid);
+            return;
         }
         else {
-            System.out.println("");
+            System.out.println("Cant train");
         }
     }
 }
